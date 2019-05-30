82bfbe5d298a7209ad048379b0d5399bcceca02e
Ricardo Oliveira <rcdoliveira98@hotmail.com>
2019-05-30 16:26:27 -0300

    OrderServices e entity

---
+++ src/modules/store/entities/order.entity.ts
@@ -0,0 +1,21 @@
+import { OneToMany, Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { OrderItem } from "./order-item.entity";
+
+@Entity()
+export class Order{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length : 8 })
+    number: string;
+    
+    @Column('datetime')
+    date: Date;
+
+    @Column({ length: 11 })
+    customer: string;
+
+    @OneToMany(() => OrderItem, (oi) => oi.order)
+    items: OrderItem[];
+}
+
